f515be4b6c5b0cb50a921db997a15addf1a5ba67
Ahmed Ismail <iamahmedismail19@gmail.com>
2024-12-03 20:43:51 +0500

    Remove project-warning related to gettext

--- lib/supermarket_web/gettext.ex
+++ lib/supermarket_web/gettext.ex
@@ -20,5 +20,5 @@ defmodule SupermarketWeb.Gettext do
 
   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
   """
-  use Gettext, otp_app: :supermarket
+  use Gettext.Backend, otp_app: :supermarket
 end
